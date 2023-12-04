9c95c7867de49a9bf2201adf3fd6bd9f40a65a8e
Sri Sakthi Krishnan T <SriSakthiKrishnan.T@resideo.com>
2023-12-04 08:03:04 +0530

    Two Sum

---
+++ LeetCode/Easy/TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://leetcode.com/problems/two-sum/
+
+//Example 1:
+//int[] nums = new int[] { 2, 7, 11, 15 };
+//int target = 9;
+
+//Example 2:
+//int[] nums = new int[] { 3, 2, 4 };
+//int target = 6;
+
+//Example 3:
+int[] nums = new int[] { 3, 3 };
+int target = 6;
+
+
+int[] output = null;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] + nums[j] == target)
+        {
+            output = new int[] {i, j};
+        }
+    }
+}
+
+Console.ReadLine();
+
+
