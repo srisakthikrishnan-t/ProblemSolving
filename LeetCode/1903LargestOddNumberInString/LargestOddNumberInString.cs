f08448acc77397c4ed86a9e7cd3992d5153f03d1
Sri Sakthi Krishnan T <SriSakthiKrishnan.T@resideo.com>
2023-12-08 13:58:50 +0530

    1903 LargestOddNumberInString

---
+++ LeetCode/1903LargestOddNumberInString/LargestOddNumberInString.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace _1903LargestOddNumberInString
+{
+    public class LargestOddNumberInString
+    {
+        public string LargestOddNumber(string num)
+        {
+            var number = string.Empty;
+            BigInteger bigOdd = 0;
+
+            for (int i = num.Length; i>0; i--)
+            {
+                if (Convert.ToInt32(num[i - 1]) % 2 != 0)
+                {
+                    number = num.Substring(0, i);
+
+                    var newInt = BigInteger.Parse(number);
+
+                    if (!newInt.IsEven && newInt > bigOdd)
+                    {
+                        bigOdd = newInt;
+                        break;
+                    }
+                }
+            }
+
+            return bigOdd > 0? bigOdd.ToString() : "" ;
+        }
+    }
+}
