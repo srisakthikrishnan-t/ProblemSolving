37ad7591b733a2cdc581d682fdecf0801021a71c
Sri Sakthi Krishnan T <SriSakthiKrishnan.T@resideo.com>
2023-12-11 12:04:56 +0530

    867TransposeMatrix

---
+++ LeetCode/867TransposeMatrix/TransposeMatrix.cs
@@ -0,0 +1,23 @@
+﻿namespace _867TransposeMatrix
+{
+    public class TransposeMatrix
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            var transpose = new int[matrix.Length][];
+
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                var tempArray = new int[matrix.Length];
+
+                for (var j = 0; j < matrix[i].Length; j++)
+                {
+                    tempArray[j] = matrix[j][i];
+                }
+
+                transpose[i] = tempArray;
+            }
+            return transpose;
+        }
+    }
+}
